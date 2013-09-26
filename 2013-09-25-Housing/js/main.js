8cd9e96025b753163f845ef0e35bb1ef8dd1d8fa
Ronald Ford <the.ford.foundation1@gmail.com>
2013-09-26 04:32:25 -0500

    unfinished housing

--- 2013-09-25-Housing/js/main.js
+++ 2013-09-25-Housing/js/main.js
@@ -1,13 +1,34 @@
 var rooms = [];
-var another = 'yes';
+var another = "";
+var window_cost = 250;
+var sqft_cost = 200
+var house = {}
 
-while(another = 'yes');
+
+function total_area()
+{
+  var sum = 0;
+  for(var i = 0; i < rooms.length; i++)
+  {
+    sum += rooms[i].area;
+  }
+  return sum;
+}
+
+
+while(another == '')
 {
   var room = {};
+  room.num = prompt("Enter amount of rooms desired.");
   room.name = prompt("What kind of room would you like?");
-  room.width = parseInt(prompt("What is the width of room?"));
-  room.length = parseInt(prompt("What is the length of room?"));
-  room.windows = parseInt(prompt("How many windows would you like?"));
+  room.width = parseFloat(prompt("What is the width of room?"));
+  room.length = parseFloat(prompt("What is the length of room?"));
+  room.number_windows = parseInt(prompt("How many windows would you like?"));
   rooms.push(room);
-  another = prompt("Would You like another room? " + another );
+  another = prompt("Press enter to add another room " + another );
 }
+
+var total_square_feet = (room.width * room.length);
+
+
+
